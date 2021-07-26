ee581bb662edd9210446458f8b5b6f879f5eca50
Ferenc Érsek <ersekferi79@gmail.com>
2021-07-27 01:30:28 +0200

    delete article

---
+++ test/delete_article.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import urllib
+
+
+browser = webdriver.Chrome("/usr/bin/chromedriver")
+browser.get("http://localhost:1667")
+
+browser.maximize_window()
+browser.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[2]/div').click()
+
+browser.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[2]/a').click()
+email = browser.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[1]/input')
+password = browser.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[2]/input')
+email.send_keys("user2@hotmail.com")
+time.sleep(1)
+password.send_keys("Userpass1")
+time.sleep(1)
+browser.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/button').click()
+time.sleep(1)
+user_name = WebDriverWait(
+    browser, 5).until(
+    EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/nav/div/ul/li[4]/a'))
+#    EC.visibility_of_element_located((By.XPATH, '//*[@class="nav-link" and text()="user2"]'))
+)
+assert user_name.text == "user2"
+print(user_name.text)
+time.sleep(1)
+print(f"BEJELENTKEZÉS: {user_name.text}")
+
+# modify post
+title_list = []
+#user = browser.find_element_by_xpath('//*[@class="nav-link" and text()="user2"]')
+
+user_name.click()
+time.sleep(2)
+old_title = WebDriverWait(
+    browser, 5).until(
+    EC.visibility_of_element_located((By.XPATH, '//*[@class="preview-link"]/h1'))
+)
+title_list.append(old_title.text)
+old_title.click()
+
+delete_article_btn = WebDriverWait(
+    browser, 5).until(
+    EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div[1]/div/div/span/button/span'))
+).click()
+
+# deleted the article. Going home
+#assert(browser.current_url == 'http://localhost:1667/#/')
+
+print(browser.current_url)
+
