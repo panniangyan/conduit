69baa22df87e76e486cffd6d6b31e7e77178ee32
Kovácsné Ángyán Annamária <66997541+panniangyan@users.noreply.github.com>
2021-08-05 18:40:37 +0200

    Update test_conduit.py

--- test/test_conduit.py
+++ test/test_conduit.py
@@ -10,10 +10,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-import os
-
-
 # # defining functions
 
 def xpath(browser, xpath_search):
